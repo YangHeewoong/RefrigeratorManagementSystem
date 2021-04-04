6870da69d6a4ced468949d36e127d1681857ae8e
YangHeewoong <didgbd@gmail.com>
2021-04-04 17:31:40 +0900

    add class

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,37 +3,39 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num = 0;
+		
 		Scanner input = new Scanner(System.in);
+		RefrigeratorManager refrigeratormanager = new RefrigeratorManager(input);
 		
-		
-			while (num != 6) {
-			System.out.println("Refrigerator Management System Menu");
+		int num = 0;
+			while (num != 5) {
+			System.out.println("<Refrigerator Management System Menu>");
 			System.out.println("1. Add Food");
 			System.out.println("2. Delete Food");
 			System.out.println("3. Edit Food");
 			System.out.println("4. View Food");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6");
+			System.out.println("5. Exit");
+			System.out.println("Select one number between 1-5");
 			
 			num = input.nextInt();
 			
 			switch(num) 
 			{
 			case 1 :
-				addFood();
+				refrigeratormanager.addFood();
 				break;
 				
 			case 2 :				
-				deleteFood();
+				refrigeratormanager.deleteFood();
 				break;
 			
 			case 3 :
-				editFood();
+				refrigeratormanager.editFood();
 				break;
 				
 			case 4 :
+				refrigeratormanager.viewFood();
+				break;
 				
 			case 5 :	
 			
@@ -41,42 +43,6 @@ public class MenuManager {
 													
 		}
 	}
-	public static void addFood() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Type of Food : "); 
-		String Food = input.next();
-		
-		System.out.print("Number of Food : ");
-		int Number = input.nextInt();
-		
-		System.out.print("Shelf life : ");
-		String Shelflife = input.next();
-		
-		System.out.print("Storage Place : "); 
-		String Storage = input.next();
-	}
-	public static void deleteFood() {
-		System.out.println("Input what you want to delete food type");
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Type of Food : ");
-		String Delete_Food = input.next();
-	}
-	public static void editFood() {
-		System.out.println("Input what you want to Edit");
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Type of Food  : "); 
-		String Edit_Food = input.next();
-		
-		System.out.print("Number of Food : ");
-		int Edit_Number = input.nextInt();
-		
-		System.out.print("Shelf life : ");
-		String Edit_Shelflife = input.next();
-		
-		System.out.print("Storage Place : "); 
-		String Edit_Storage = input.next();
-	}
+
 
 }
